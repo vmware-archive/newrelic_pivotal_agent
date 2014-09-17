52dd1a9af3bbedef7e1749c60452cea930279eb3
Wes Schlichter <wschlichter@pivotal.io>
2014-09-17 10:32:15 -0500

    Fix rabbitmq debug loggin

--- plugins/pivotal_rabbitmq_plugin/pivotal_rabbitmq_plugin.rb
+++ plugins/pivotal_rabbitmq_plugin/pivotal_rabbitmq_plugin.rb
@@ -35,7 +35,7 @@ module NewRelic
     class Agent < NewRelic::Plugin::Agent::Base
       agent_guid 'com.pivotal.newrelic.plugin.rabbitmq'
       agent_version '1.0.5'
-      agent_config_options :management_api_url
+      agent_config_options :management_api_url, :debug
       agent_human_labels('RabbitMQ') do
         uri = URI.parse(management_api_url)
         "#{uri.host}:#{uri.port}"
