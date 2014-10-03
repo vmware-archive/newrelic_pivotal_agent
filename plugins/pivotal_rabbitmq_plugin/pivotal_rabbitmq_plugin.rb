69e45d26ed24bfb86a8b8588abd901bc148190bb
Sebastian Ramirez <sebastian.rmirz@gmail.com>
2014-10-02 23:09:09 -0700

    Do not report metrics for temporary queues.

--- plugins/pivotal_rabbitmq_plugin/pivotal_rabbitmq_plugin.rb
+++ plugins/pivotal_rabbitmq_plugin/pivotal_rabbitmq_plugin.rb
@@ -157,6 +157,7 @@ module NewRelic
       def report_queues
         return unless rmq_manager.queues.length > 0
         rmq_manager.queues.each do |q|
+          next if q['name'].start_with?('amq.gen')
           report_metric_check_debug 'Queue' + q['vhost'] + q['name'] + '/Messages/Ready', 'message', q['messages_ready']
           report_metric_check_debug 'Queue' + q['vhost'] + q['name'] + '/Memory', 'bytes', q['memory']
           report_metric_check_debug 'Queue' + q['vhost'] + q['name'] + '/Messages/Total', 'message', q['messages']
